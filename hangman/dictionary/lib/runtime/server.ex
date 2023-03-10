6d65fc6550291bc4f6d995f68286340c3b7b5096
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-03-09 19:20:02 -0600

    Turn dictionary into an Elixir app.

--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -2,11 +2,13 @@ defmodule Dictionary.Runtime.Server do
 
   @type t :: pid()
 
+  @proc __MODULE__
+
   alias Dictionary.Impl.WordList
 
-  def start_link, do: Agent.start_link(&WordList.word_list/0)
+  def start_link, do: Agent.start_link(&WordList.word_list/0, name: @proc)
 
-  def random_word(pid) do
-    Agent.get(pid, &WordList.random_word/1)
+  def random_word() do
+    Agent.get(@proc, &WordList.random_word/1)
   end
 end
