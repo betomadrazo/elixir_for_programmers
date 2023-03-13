16ac5caf36e1ad84a31004fe983ab03237c80128
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-03-13 07:02:28 -0600

    Dave Thomas's solution to the Fibonacci with cache exercise.

--- hangman/dictionary/lib/runtime/application.ex
+++ hangman/dictionary/lib/runtime/application.ex
@@ -1,6 +1,7 @@
 defmodule Dictionary.Runtime.Application do
 
-  use Application
+  use Application # this is a behavior,
+  # includes a bunch of defaults into this module
 
   def start(_type, _args) do
     Dictionary.Runtime.Server.start_link()
