65380244f9d7e224f750ab9962ddb85d1b3ef4af
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-03-16 06:46:04 -0600

    Finished section 12. Supervisors, Agents, processes, separation of runtime/compiletime, etc.

--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -4,11 +4,16 @@ defmodule Dictionary.Runtime.Server do
 
   @proc __MODULE__
 
+  use Agent
+
   alias Dictionary.Impl.WordList
 
-  def start_link, do: Agent.start_link(&WordList.word_list/0, name: @proc)
+  def start_link(_), do: Agent.start_link(&WordList.word_list/0, name: @proc)
 
   def random_word() do
+    # if :rand.uniform < 0.33 do
+    #   Agent.get(@proc, fn _ -> exit(:boom) end)
+    # end
     Agent.get(@proc, &WordList.random_word/1)
   end
 end
