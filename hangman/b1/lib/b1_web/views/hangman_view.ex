4b90aca7e83e8bab6365988da4fcdef330a2f58d
Alberto Madrazo <alberto.madrazo@coppel.com>
2023-03-24 12:45:51 -0600

    Refactoring and some rearrangement of the view. Add functions to show the hangman stages.

--- hangman/b1/lib/b1_web/views/hangman_view.ex
+++ hangman/b1/lib/b1_web/views/hangman_view.ex
@@ -1,3 +1,115 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
+
+  def figure_for(0) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+     /|\\   |
+     / \\   |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(1) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+     /|\\   |
+     /     |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(2) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+     /|\\   |
+           |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(3) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+     /|    |
+           |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(4) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+      |    |
+           |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(5) do
+    ~s{
+      ______
+      |    |
+      |    |
+      O    |
+           |
+           |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(6) do
+    ~s{
+      ______
+      |    |
+      |    |
+           |
+           |
+           |
+           |
+           |
+    ========
+    }
+  end
+
+  def figure_for(7) do
+    ~s{
+      ______
+      |    |
+           |
+           |
+           |
+           |
+           |
+           |
+    ========
+    }
+  end
 end
