8c82ebaeb838e94079892583e9f0007260acbcd6
Alberto Madrazo <betomadrazo@gmail.com>
2023-03-24 19:12:22 -0600

    Extract from the template to helpers in the view; do styling of things.

--- hangman/b1/lib/b1_web/views/hangman_view.ex
+++ hangman/b1/lib/b1_web/views/hangman_view.ex
@@ -1,6 +1,35 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
 
+  def continue_or_try_again(conn, status) when status in [ :won, :lost ] do
+    button("Try again", to: Routes.hangman_path(conn, :new))
+  end
+
+  def continue_or_try_again(conn, _) do
+    form_for(conn, Routes.hangman_path(conn, :update), [ as: "make_move", method: :put, class: ["form-guess"] ],
+      fn f ->
+        [
+          text_input(f, :guess, style: "margin-right: 12px; margin-bottom: 0"),
+          submit("Make next guess"),
+        ]
+      end
+    )
+  end
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter at a time" },
+    good_guess:   { "good-guess",   "Good guess!" },
+    bad_guess:    { "bad-guess",    "Sorry, that's a bad guess" },
+    won:          { "won",          "You won!" },
+    lost:         { "lost",         "Sorry, you lost" },
+    already_used: { "already-used", "You already used that letter" }
+  }
+
+  def move_status(status) do
+    { class, msg } = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
   def figure_for(0) do
     ~s{
       ______
