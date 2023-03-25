fb3be9fee468eb0d2106a69fb2c514a6324fe80e
Alberto Madrazo <betomadrazo@gmail.com>
2023-03-25 17:42:16 -0600

    Make alphabet component come to life.

--- hangman/b2/lib/b2_web/live/game/alphabet.ex
+++ hangman/b2/lib/b2_web/live/game/alphabet.ex
@@ -3,12 +3,29 @@ defmodule B2Web.Live.Game.Alphabet do
   use B2Web, :live_component
 
   def mount(socket) do
-    { :ok, socket }
+    letters = (?a..?z) |> Enum.map(&<< &1 :: utf8 >>)
+    { :ok, assign(socket, :letters, letters) }
   end
 
   def render(assigns) do
     ~L"""
-    In Alphabet
+    <div class="alphabet">
+    <%= for letter <- assigns.letters do %>
+      <span phx-click="make_move"
+            phx-value-key="<%= letter %>"
+            class='one-letter <%= class_of(letter, @tally) %>'>
+        <%= letter %>
+      </span>
+    <% end %>
+    </div>
     """
+  end
+
+  defp class_of(letter, tally) do
+    cond do
+      Enum.member?(tally.letters, letter) -> "correct"
+      Enum.member?(tally.used, letter) -> "wrong"
+      true -> ""
+    end
   end
 end
